92cf2c7e09a07619e6031ba7815e454f87ddf4ce
ampjimen <ampjimen@student.42madrid.com>
2024-02-26 16:34:46 +0100

    anotaciones

--- execve.c
+++ execve.c
@@ -9,7 +9,11 @@ int main()
 	args[1] = "-l";
 	args[2] = NULL;
 	execve("/bin/ls", args, NULL);
+	/*execve ejecuta un programa en el espacio de memoria
+	del proceso actual. En este caso, se intenta ejecutar ls
+	ubicado en bin/ls*/
 	printf("This line will not be executed.\n");
-
+	/*Esta linea no se debería ejecutar, ya que hemos reemplazado 
+	el programa actual por el que hemos especificado.*/
 	return (0);
 }
