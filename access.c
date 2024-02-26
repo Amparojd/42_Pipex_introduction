1bf110ff0b6602327cd6f303223430c6418bafbe
ampjimen <ampjimen@student.42madrid.com>
2024-02-26 16:29:04 +0100

    anotaciones

--- access.c
+++ access.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
- 
+
+
+/*Se verifica con access que example.txt es 
+accesible para lectura (R_OK)*/
+
 int main()
 {
-	if (access("example.txt", R_OK) != -1)
+	if (access("example.txt", R_OK) != -1) //-1 es error
 		printf("I have permission\n");
 	else
 		printf("I don't have permission\n");
--- dub2.c
+++ dub2.c
@@ -4,11 +4,23 @@
  
 int main()
 {
-	int fd;
+	int fd; //variable para almacenar el file descriptor
  
 	fd = open("example.txt", O_WRONLY | O_CREAT, 0644);
+	//abrimos O_WRONLY (escritura) si no existe, O_CREAT (lo crea)
+	//0644 ver permisos Unix
 	dup2(fd, STDOUT_FILENO);
+	//dup2 duplica el fd en STDOUT_FILENO (fd estandar de salida)
+	//Cualquiera salida que se enviaría a la consola (STDOUT) ahora se
+	//enviará al archivo "example.txt", porque lo redirecciona
 	close(fd);
+	//close cierra el fd original porque no se necesita, ya que hemos
+	//redirigido a través de STDOUT_FILENO
 	printf("This is printed in example.txt!\n");
 	return (0);
 }
+
+/*Permisos UNIX
+Lectura (r)
+Escritura(w)
+Ejecución(x)*/
